550e4a6a2e3ab14dfe19169a3145d1963458947f
anaskhonprime <smlife202@gmail.com>
2023-03-05 15:51:39 +0900

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+/* Importing  all Components */
+import Home from "./components/Home";
+import Blogs from "./components/Blogs";
+import Projects from "./components/Projects";
+import About from "./components/About";
+import NotFound from "./components/404";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/blogs" element={<Blogs />}></Route>
+          <Route path="/Projects" element={<Projects />}></Route>
+          <Route path="/about" element={<About />}></Route>
+
+          <Route path="*" element={<NotFound />}></Route>
+
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
