73d139d265cba97a2bea8ace7499111dc23f2ff6
anaskhonprime <smlife202@gmail.com>
2023-03-25 22:01:33 +0900

    Updated Resume Components

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,7 +3,13 @@ import { Link } from "react-router-dom";
 
 import "../style/resume.css";
 import { CodeSVG } from "./Svg/SvgIcons.js";
-import { resumeHeader, Intro } from "../data/resume.js";
+import {
+  resumeHeader,
+  Intro,
+  beckend,
+  frontend,
+  tools,
+} from "../data/resume.js";
 
 function Resume() {
   return (
@@ -48,11 +54,115 @@ function Resume() {
 
           <div className="row intro-body">
             <div className="row intro-header">
-              <div className="col-lg-1 ">
+              <div className="col-lg-1">
                 <CodeSVG />
               </div>
               <div className="col-lg-11 main-text">
                 <h4>About Me</h4>
+              </div>
+              <hr />
+            </div>
+            <div className="col-lg-5 intro-text">
+              <h5>Introduction</h5>
+            </div>
+            <div className="col-lg-7 my-list">
+              <ul>
+                {Intro.map((key, a) => (
+                  <div key={a} className="list">
+                    <li>{Intro[a].list}</li>
+                  </div>
+                ))}
+              </ul>
+            </div>
+          </div>
+
+          <div className="row intro-body ">
+            <div className="col-lg-5  intro-text">
+              <h5>Contact & Blogs</h5>
+            </div>
+            <div className="col-lg-7 my-list">
+              <ul>
+                <li className="text-underline">
+                  Email : <span> smlife202@gmail.com</span>
+                </li>
+                <li>
+                  Github :
+                  <a href="/" target="_blank">
+                    https://github.com/anaskhonprime
+                  </a>
+                </li>
+                <li>
+                  Blogs :
+                  <a href="/" target="_blank">
+                    https://prmblogs.tistory.com/
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="row intro-body ">
+            <div className="row intro-header">
+              <div className="col-lg-1">
+                <CodeSVG />
+              </div>
+              <div className="col-lg-11 main-text">
+                <h4>Skills</h4>
+              </div>
+              <hr />
+            </div>
+
+            <div className="col-lg-5 intro-text">
+              <h5>Frontend</h5>
+            </div>
+
+            <div className="col-lg-7  my-list">
+              <ul>
+                {frontend.map((key, a) => (
+                  <div key={a} className="list">
+                    <li>{frontend[a].list}</li>
+                  </div>
+                ))}
+              </ul>
+            </div>
+            <div className="free-space"></div>
+            <div className="col-lg-5 intro-text">
+              <h5>Backend</h5>
+            </div>
+
+            <div className="col-lg-7  my-list">
+              <ul>
+                {beckend.map((key, a) => (
+                  <div key={a} className="list">
+                    <li>{beckend[a].list}</li>
+                  </div>
+                ))}
+              </ul>
+            </div>
+
+            <div className="free-space"></div>
+            <div className="col-lg-5 intro-text">
+              <h5>Tools & Software</h5>
+            </div>
+
+            <div className="col-lg-7  my-list">
+              <ul>
+                {tools.map((key, a) => (
+                  <div key={a} className="list">
+                    <li>{tools[a].list}</li>
+                  </div>
+                ))}
+              </ul>
+            </div>
+          </div>
+
+          <div className="row intro-body">
+            <div className="row intro-header">
+              <div className="col-lg-1">
+                <CodeSVG />
+              </div>
+              <div className="col-lg-11 main-text">
+                <h4>Work Experience & Projects</h4>
               </div>
               <hr />
             </div>
--- src/data/resume.js
+++ src/data/resume.js
@@ -23,5 +23,64 @@ export const Intro = [
   },
   {
     list: "하루 동안 배운 새로운 내용들 깃헙으로 커밋하고 블로그로 기록하는 습관을 앞으로도 꾸준히 해나갈 생각입니다.",
-  }
+  },
 ];
+
+export const beckend = [{ list: "NodeJs" }, { list: "ExpressJs" }];
+
+export const frontend = [
+  { list: "HTML5, CSS3, JavaScript" },
+  { list: "JQuery, Bootstrap, TailwindCss" },
+  { list: "ReactJs" },
+  { list: "VueJs" },
+  { list: "TypeScript(Basic)" },
+];
+
+export const tools = [
+  { list: "VsCode, Git" },
+  { list: "Figma, AdobeXD" },
+  { list: "Firebase, Github" },
+];
+
+export const work1 = {
+  place: "제이투모로우원",
+  job: "인턴",
+  time: "2021. 09 ~ 2021. 12",
+  experience1: "반응형 홈페이지 다자인 및 개발",
+  experience2: "UI/UX 디자인",
+  experience3: "사용자 데이터베이스 설계 및 관리",
+  point: "첫 회사 생활 시작, 앞으로 공부 방향을 잘 잡을 수 있는경험",
+  review:
+    "공부와 실제 일에 대한 차이점, 시간 약속과 의사소통의 중요성, 맡긴 업무에 대한 책임",
+  unfortunate: "생각보다 짧은 활동 기간, 서로 의사소통"
+};
+
+export const work2 = {
+  place: "전남대학교",
+  job: "서포터즈 | 홍보팀",
+  time: "2021. 03 ~ 2022. 03",
+  experience1: "전남대학교 외국인 대상으로 다양한 행사 기획 및 운영",
+  experience2: "홍보팀으로 카드 뉴스와 포스터 제작, 영상 편집",
+  experience3: "외국인 유학생들의 교류 화동",
+  point:
+    "어떤 일에 대한 기획,홍보와 운영 과정에 대한 경험, 한국인 팀원들과 팀워크 경험",
+  review:
+    "홍보물을 제작하면서 사용자에게 정보 전달과 디자인 작업 과정에서 색, 폰트, 추가 요소들 선택에 대한 경험",
+  unfortunate: "",
+};
+
+export const work3 = {
+  place: "재택근무",
+  job: "프리랜서 ",
+  time: "2022. 04 ~ 2023. 02",
+  experience1: "블록체인 & 스마트 컨트랙트 분야에서 프리렌서 활동",
+  experience2: "10개 이상 작은 NFT 프로젝트들 성공적으로 완성",
+  experience3:
+    "이더리움, 폴리곤 기반으로 스마트 컨트랙트 개발, 메타데이터, 민팅페이지 제작 ",
+  point:
+    "다양한 프로젝트를 운영하면서 자세한 상담과 기획, 일정관리와 수많은 수정 사항에 대한 경험",
+  review:
+    "새로운 것에 도전과 경험이 없어도 시간을 몇배 투자해서 꾸준히 노력하면 성공할 수 있다는 자신이 생겼습니다.",
+  unfortunate: "경제적인 여유, 자유로운 출퇴근 등 장점이 있지만 부족한 팀워트와 사회생활, 같은 분야 사람들과 네트워팅 부분이 개인적으로 아쉬웠습니다. ",
+};
+
